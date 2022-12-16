c40416eaaf5f8ffbdb9a9cc538eaa77932277f1d
TD2007 <trinhvandung123456@gmail.com>
2022-12-16 23:09:51 +0700

    HomePage Design 90%
    Nav Bar: Category Modal, Search Bar, Login Button
    Slide show with demo Image

--- JukeBox/mvc/views/Home.php
+++ JukeBox/mvc/views/Home.php
@@ -20,34 +20,102 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="side-bar col-sm-3 col-md-3 col-lg-3 d-flex justify-content-center align-items-center flex-column">
-                <div class="logo">
-                    <img src="./public/images/logo1.png" alt="Logo">
-                    <h4>Music for Free</h4>
-                </div>
-                <hr>
-                <ul class="side-menu list-unstyled">
-                    <li class="active"><a href="#"><i class="fa-solid fa-house"></i> Home</a></li>
-                    <li class=""><a href="#"><i class="fa-solid fa-fire"></i> Popular</a></li>
-                    <li class=""><a href="#"><i class="fa-solid fa-list"></i> Playlist</a>
-                        <ul class="sub-list">
-                            <li><a href="">Daily Mix</a></li>
-                            <li><a href="">My Playlist</a></li>
-                        </ul>
-                    </li>
-                    <li id="ranking-board" class="">
-                        <h5 class=""><i class="fa-solid fa-trophy"></i> Ranking Board</h5>
-                        <a href="" class="">See more ></a>
-                    </li>
-                </ul>
-
+    <div class="row">
+        <div class="side-bar col-sm-3 col-md-3 col-lg-3 d-flex flex-column align-items-center" style="height:100%">
+            <div class="logo">
+                <img src="./public/images/logo2.png" alt="Logo">
             </div>
-            <div class="content col-sm-9 col-md-9 col-lg-9">
+            <hr>
+            <ul class="side-menu list-unstyled">
+                <li class="active"><a href="#"><i class="fa-solid fa-house"></i> Home</a></li>
+                <li class=""><a href="#"><i class="fa-solid fa-fire"></i> Popular</a></li>
+                <li class=""><a href="#"><i class="fa-solid fa-list"></i> Playlist</a>
+                    <ul class="sub-list">
+                        <li><a href="">Daily Mix</a></li>
+                        <li><a href="">My Playlist</a></li>
+                    </ul>
+                </li>
+                <li id="ranking-board" class="">
+                    <h5 class="p-0 m-1"><i class="fa-solid fa-trophy"></i> Ranking Board</h5>
+                    <a href="" class="m-1">See more ></a>
+                </li>
+            </ul>
 
+        </div>
+        <div class="col-sm-9 col-md-9 col-lg-9" id="content">
+            <nav class="navbar">
+                <div class="container-fluid p-0">
+                    <a href="#" class="fs-4 btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" id="btnCategory">Category</a>
+                    <div class="search">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                        <input type="text" placeholder="Search Music...">
+                    </div>
+                    <div class="user">
+                        <a href="Login.php" class="fs-4"><i class="fa-solid fa-circle-user"></i> Login</a>
+                    </div>
+                </div>
+            </nav>
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="./public/images/img1.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>First slide label</h5>
+                        <p>Some representative placeholder content for the first slide.</p>
+                    </div>
+                    </div>
+                    <div class="carousel-item">
+                    <img src="./public/images/img2.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Second slide label</h5>
+                        <p>Some representative placeholder content for the second slide.</p>
+                    </div>
+                    </div>
+                    <div class="carousel-item">
+                    <img src="./public/images/img3.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Third slide label</h5>
+                        <p>Some representative placeholder content for the third slide.</p>
+                    </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
             </div>
         </div>
+    </div>  
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Category</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <ul class="fs-4 list-unstyled d-flex flex-row" id="categoryList">
+                <li><a href="">US-UK</a></li>
+                <li><a href="">Vpop</a></li>
+                <li><a href="">Kpop</a></li>
+                <li><a href="">Cpop</a></li>
+                <li><a href="">Jpop</a></li>
+            </ul>
+        </div>
+        <div class="modal-footer">
+        </div>
+        </div>
+    </div>
     </div>
 </body>
 </html>
